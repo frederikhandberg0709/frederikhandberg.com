3b898a64856038e7f9a3cef2ffe8ce1f5ea10357
FREDERIK DIAMOND <hello@frederikhandberg.com>
2025-03-08 21:42:33 +0100

    Adding text for social network portfolio page

--- src/app/portfolio/social-network/page.tsx
+++ src/app/portfolio/social-network/page.tsx
@@ -3,6 +3,12 @@ export default function SocialNetwork() {
     <div className="flex min-h-screen flex-col items-center justify-start">
       <div className="flex w-full max-w-7xl flex-col gap-5">
         <h1 className="text-3xl font-bold">Social Network</h1>
+        <p>
+          My social network is a fully functional web application developed with
+          Next.js. It incorporates most of the essential features you&apos;d
+          expect from a modern social network, including post creation, liking,
+          sharing, commenting, and replying.
+        </p>
       </div>
     </div>
   );
