12ded73fea076b13a2db5b456546c9f1576a4600
FREDERIK DIAMOND <hello@frederikhandberg.com>
2025-04-22 20:55:17 +0200

    Starting to work on the blog section

---
+++ src/components/sections/Blog.tsx
@@ -0,0 +1,12 @@
+export default function Blog() {
+  return (
+    <div>
+      <h2 className="text-center text-lg font-bold tracking-wider">BLOG</h2>
+      <p className="text-sm">
+        On my short-form blog, I write about the software projects I&apos;m
+        working on. I share the progress I make and the struggles I face along
+        the way.
+      </p>
+    </div>
+  );
+}
